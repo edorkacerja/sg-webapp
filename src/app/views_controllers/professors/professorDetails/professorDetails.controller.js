2f3cf984200e0f3b0a7700763885dca03d3afb5c
Edor Kacerja <edorkacerja@gmail.com>
2016-04-29 00:20:18 +0300

    editing professors

--- src/app/views_controllers/professors/professorDetails/professorDetails.controller.js
+++ src/app/views_controllers/professors/professorDetails/professorDetails.controller.js
@@ -2,16 +2,39 @@ const SERVICE = new WeakMap();
 
 export class ProfessorDetailsController {
 
-  constructor($modal, $scope, $stateParams, ProfessorsService) {
+  constructor($modal, $scope, $stateParams, ProfessorsService, $state) {
     'ngInject';
     this.$scope = $scope;
     this.$stateParams = $stateParams;
     this.$modal = $modal;
+    this.$state = $state;
     SERVICE.set(this, ProfessorsService.resource);
 
-
-
+    this.professorDetails = {};
     this.getProfessor(this.$stateParams.professorId);
+
+
+    this.toDataUrl('http://s3-eu-central-1.amazonaws.com/aubg-sg/professors/photos/000/000/012/original/something.jpg?1461872491', (base64Img) => {
+      this.professorDetails.image_json = base64Img;
+    });
+
+  }
+
+  toDataUrl(url, callback, outputFormat) {
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function () {
+      var canvas = document.createElement('CANVAS');
+      var ctx = canvas.getContext('2d');
+      var dataURL;
+      canvas.height = this.height;
+      canvas.width = this.width;
+      ctx.drawImage(this, 0, 0);
+      dataURL = canvas.toDataURL(outputFormat);
+      callback(dataURL);
+      canvas = null;
+    };
+    img.src = url;
   }
 
 
@@ -24,21 +47,21 @@ export class ProfessorDetailsController {
 
 
   saveProfessorEditing(professorId) {
-    console.log('editing');
-    SERVICE.get(this).update({professorId: professorId}).$promise.then(() => {});
+    console.log('------------');
+    delete this.professorDetails.photo;
+    var professorObj = JSON.parse('{"professor":' + JSON.stringify(this.professorDetails) + '}');
+    SERVICE.get(this).update({professorId: professorId}, professorObj).$promise.then(() => {
+    });
   }
 
-  deleteProfessor(professorId){
 
-    this.$modal.open({
-      animation: true,
-      template: "<p> edor</p>",
-      controller: 'AddProfessorController',
-      controllerAs: 'apr',
-      size: 'md'
+  deleteProfessor(professorId) {
+    SERVICE.get(this).delete({professorId: professorId}).$promise.then((result)=> {
+      this.$state.go('professors');
+      console.log('finally deleted');
+    }, (error)=> {
+      console.log(error);
     });
-
-    SERVICE.get(this).delete({professorId: professorId});
   }
 
 
